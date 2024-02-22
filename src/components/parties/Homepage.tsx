92828307228451febac235fe6330983cabecceea
Priyanka Gautam <priyankagautam@Priyankas-MacBook-Pro.local>
2024-02-21 23:38:18 -0700

    raw version of the bottom nav bar. but it is working

--- src/components/parties/Homepage.tsx
+++ src/components/parties/Homepage.tsx
@@ -14,7 +14,7 @@ import {
   Text
 } from "@gluestack-ui/themed";
 import { config } from "@gluestack-ui/config";
-import BottomNavBar from "../NavBar/BottomNavBar";
+
 import MyParties from "../parties/MyParties";
 import ProfilePage from "../profilepage/ProfilePage";
 import FontAwesome from '@expo/vector-icons/FontAwesome';
