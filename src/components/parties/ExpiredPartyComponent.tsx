3908f5eefd4d10147471e12a9eca6a8bb841e402
Nicole Zacaruk <nicole.zacaruk@gmail.com>
2024-03-08 22:47:55 -0700

    Added fab menu but menu is behind other elements

--- src/components/parties/ExpiredPartyComponent.tsx
+++ src/components/parties/ExpiredPartyComponent.tsx
@@ -1,54 +1,41 @@
 import React, { useEffect, useState } from "react";
-import { View, StyleSheet, TouchableOpacity } from "react-native";
+import { View, StyleSheet, TouchableOpacity, ScrollView } from "react-native";
 import {  useNavigation } from "@react-navigation/native";
 import {
-  GluestackUIProvider,
-  SafeAreaView,
-  VStack,
   Box,
   Image,
-  Heading,
-  Button,
-  ButtonText,
-  ButtonIcon,
-  ButtonGroup,
   Text,
-  HStack,
-  Badge,
-  BadgeText,
 } from "@gluestack-ui/themed";
 import { config } from "@gluestack-ui/config";
 
 const ExpiredPartyComponent = ({partyName= "My Party"}) => {
     return (
-        <TouchableOpacity onPress={()=>{console.log('my party')}}>
-            <Box
-                bg="#DDDDDD"
-                py="$4"
-                px="$3"
-                // height="$200"
-                width="$40"
-                maxWidth="100%"
-                rounded="$md"
-                borderColor="#878080"
-                borderWidth={0.5}
-                style={styles.badge}
-                marginBottom="$3"
-                >
-                <View style={styles.safetyIcon} >
-                    <Image
-                    alt="shiedl"
-                    source={require("../../../assets/location.png")}
-                    style={{width: 50, height: 50}}
-                    opacity={0.6}
-                    />
-                </View>
-                <View style ={styles.textContent}>
-                    <Text bold={true}>My Party</Text>
-                    <Text fontSize="$xs" >expired</Text>
-                </View>
-        </Box>
-    </TouchableOpacity>
+        <Box
+            bg="#DDDDDD"
+            py="$4"
+            px="$3"
+            // height="$200"
+            width="$40"
+            maxWidth="100%"
+            rounded="$md"
+            borderColor="#878080"
+            borderWidth={0.5}
+            style={styles.badge}
+            marginBottom="$3"
+            >
+            <View style={styles.safetyIcon} >
+                <Image
+                alt="shiedl"
+                source={require("../../../assets/location.png")}
+                style={{width: 50, height: 50}}
+                opacity={0.6}
+                />
+            </View>
+            <View style ={styles.textContent}>
+                <Text bold={true}>My Party</Text>
+                <Text fontSize="$xs" >expired</Text>
+            </View>
+    </Box>
     )
 };
 export default ExpiredPartyComponent;
--- src/components/parties/FabMenu.tsx
+++ src/components/parties/FabMenu.tsx
@@ -1,22 +1,26 @@
 import React, { useEffect, useState } from "react";
-import { View, StyleSheet, TouchableOpacity } from "react-native";
+import { View, StyleSheet, Pressable, TouchableOpacity } from "react-native";
 import {  useNavigation } from "@react-navigation/native";
 import {
-  GluestackUIProvider,
-  SafeAreaView,
-  Box,
-  Image,
-  Heading,
+  VStack,
   Text,
   Fab,
   FabIcon,
   MenuIcon,
+  Heading,
 } from "@gluestack-ui/themed";
 import { config } from "@gluestack-ui/config";
 
 const FabMenu = () => {
+    const [menuVisible, setVisible] = useState(false);
+    const handleMenuItemPress = (item) => {
+        // Handle menu item press here
+        console.log("Pressed:", item);
+        // Close menu after item is pressed
+        setVisible(false);
+      };
     return (
-
+    <View>
         <Fab
             size="lg"
             placement="top right"
@@ -24,12 +28,59 @@ const FabMenu = () => {
             isDisabled={false}
             isPressed={false}
             bgColor="#14543D"
+            style={styles.fab}
         >
-            <FabIcon as={MenuIcon} mr="$0" />
+            <Pressable onPress={()=>{setVisible(!menuVisible)}}>
+                <FabIcon as={MenuIcon} mr="$0" />
+            </Pressable>
+            
         </Fab>
+        {menuVisible && (
+            <View style={styles.menu}>
+                <TouchableOpacity onPress={() => handleMenuItemPress("Item 1")} style={styles.menuItem}>
+                    <Text>o   Create New Party</Text>
+                </TouchableOpacity>
+                <TouchableOpacity onPress={() => handleMenuItemPress("Item 2")} style={styles.menuItem}>
+                    <Text>o   Join A Party</Text>
+                </TouchableOpacity>
+                {/* Add more menu items as needed */}
+            </View>
+        )} 
+        {menuVisible && (
+            <TouchableOpacity style={styles.overlay} onPress={()=>{setVisible(false)}} />
+        )}
+    </View>
     )
 };
 export default FabMenu;
 
-
+const styles = StyleSheet.create({ 
+    fab: {
+        top: -40,
+    },
+    background:{
+        position:"absolute",
+        backgroundColor: "red",
+        width: 800,
+        height: 1500,
+    },
+    menu: {
+        position: 'absolute',
+        bottom: -150,
+        right: 20,
+        backgroundColor: 'white',
+        borderRadius: 8,
+        padding: 15,
+        zIndex: 100,
+      },
+      menuItem: {
+        padding: 15,
+        zIndex: 101,
+      },
+      overlay: {
+        zIndex: 5,
+        ...StyleSheet.absoluteFillObject,
+        backgroundColor: 'rgba(100, 100, 100, 0.5)',
+      },
+})
 
